ac898fab12f3128a11c06c441edb398abe07e412
admin <admin@kwb.org>
2024-10-07 16:09:44 -0700

    Added loader, finshed script

--- metro_etl.py
+++ metro_etl.py
@@ -1,4 +1,5 @@
 import polars, os
+from loader import load
 
 old_file = "data_dump/Well Water Depth.csv"
 new_file = "data_dump/cleaned_metro_data.csv"
@@ -16,29 +17,23 @@ def remove_unneeded_data():
 
 def transform_data(remove_unneeded_data):
     new_file = remove_unneeded_data()
-
-    return (
-        polars.read_csv(new_file)
-        .drop(["Pool1", "Project1"])
-        .rename(
-            {
-                "FacilityName1": "State Well Number",
-                "Textbox56": "Reporting Date",
-                "Textbox57": "Measurement",
-            }
-        )
-        .drop_nulls(subset="Measurement")
-        .write_csv(new_file)
-    )
-
-
-def load_data(cleaned_data):
-    print("Done")
-    return None
+    polars.read_csv(new_file).drop(["Pool1", "Project1"]).rename(
+        {
+            "FacilityName1": "State Well Number",
+            "Textbox56": "Reporting Date",
+            "Textbox57": "Measurement",
+        }
+    ).drop_nulls(subset="Measurement").write_csv(new_file)
 
 
 if __name__ == "__main__":
 
-    cleaned_data = transform_data(remove_unneeded_data)
+    transform_data(remove_unneeded_data)
+    db_name = "kwb"
+    schema = "monitoring_well_data"
+    table_name = "metro_water_depths_post_2020"
+    prim_key = "state_well_number, reading_date"
 
-    load_data(cleaned_data)
+    load(db_name, schema, table_name, new_file, prim_key)
+
+    os.remove(new_file)
