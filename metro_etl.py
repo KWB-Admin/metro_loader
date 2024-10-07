a911e927df80986a2e357d39ab2b7cdac4a18b1d
admin <admin@kwb.org>
2024-10-07 16:11:40 -0700

    Moved variables to global

--- metro_etl.py
+++ metro_etl.py
@@ -4,6 +4,17 @@ from loader import load
 old_file = "data_dump/Well Water Depth.csv"
 new_file = "data_dump/cleaned_metro_data.csv"
 
+db_name = "kwb"
+schema = "monitoring_well_data"
+table_name = "metro_water_depths_post_2020"
+prim_key = "state_well_number, reading_date"
+
+new_col_names = {
+    "FacilityName1": "state_well_number",
+    "Textbox56": "reading_date",
+    "Textbox57": "measurement",
+}
+
 
 def remove_unneeded_data():
     with open(old_file, "r") as file_input:
@@ -18,21 +29,13 @@ def remove_unneeded_data():
 def transform_data(remove_unneeded_data):
     new_file = remove_unneeded_data()
     polars.read_csv(new_file).drop(["Pool1", "Project1"]).rename(
-        {
-            "FacilityName1": "State Well Number",
-            "Textbox56": "Reporting Date",
-            "Textbox57": "Measurement",
-        }
+        new_col_names
     ).drop_nulls(subset="Measurement").write_csv(new_file)
 
 
 if __name__ == "__main__":
 
     transform_data(remove_unneeded_data)
-    db_name = "kwb"
-    schema = "monitoring_well_data"
-    table_name = "metro_water_depths_post_2020"
-    prim_key = "state_well_number, reading_date"
 
     load(db_name, schema, table_name, new_file, prim_key)
 
