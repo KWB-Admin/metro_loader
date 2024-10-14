0d8f35e3decc2f83d07243f8cfe7c057b0451825
admin <admin@kwb.org>
2024-10-14 11:59:21 -0700

    Added logging, more documentation

--- metro_etl.py
+++ metro_etl.py
@@ -1,8 +1,22 @@
-import polars, os
+import polars, os, logging
 from kwb_loader import loader
+from datetime import datetime
 
-old_file = "data_dump/Well Water Depth.csv"
-new_file = "data_dump/cleaned_metro_data.csv"
+logging.basicConfig(
+    filename="log/metro_etl.log",
+    encoding="utf-8",
+    filemode="a",
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+old_csv = "data_dump/Well Water Depth.csv"
+new_csv = "data_dump/metro_data_without_delivery.csv"
+transformed_parquet = "data_dump/metro_data_ready_to_load.parquet"
 
 db_name = "kwb"
 schema = "kcwa"
@@ -21,29 +35,51 @@ def remove_unneeded_data():
     This take the metro csv file and writes a new file without
     the excess data spit out by metro pertaining to Delivery
     """
-    with open(old_file, "r") as file_input:
-        with open(new_file, "w") as output:
+    with open(old_csv, "r") as file_input:
+        with open(new_csv, "w") as output:
             for line in file_input:
                 if "Delivery" in line:
                     break
                 output.write(line)
+    logger.info("Successfully removed Delivery Data")
 
 
-def transform_data(remove_unneeded_data):
+def transform_data():
     """
-    This take the new csv file and overwrites it with the correct
+    This take the new csv file and creates a parquet with the correct
     columns
     """
-    remove_unneeded_data()
-    polars.read_csv(new_file).drop(["Pool1", "Project1"]).rename(
-        new_col_names
-    ).drop_nulls(subset="measurement").write_csv(new_file)
+    try:
+        data = (
+            polars.read_csv(new_csv)
+            .drop(["Pool1", "Project1"])
+            .rename(new_col_names)
+            .drop_nulls(subset="measurement")
+            .with_columns(polars.lit(datetime.today()).alias("date_added"))
+        )
+        logger.info("Successfully transformed data")
+    except:
+        logging.exception("")
+    data.write_parquet(transformed_parquet)
+    logger.info("Successfully wrote .parquet file")
 
 
 if __name__ == "__main__":
+    logger.info(
+        "--------------- Metro ETL ran on %s ----------------" % (datetime.today())
+    )
 
-    transform_data(remove_unneeded_data)
+    remove_unneeded_data()
 
-    loader.load(db_name, schema, table_name, new_file, prim_key)
+    transform_data()
 
-    os.remove(new_file)
+    loader.load(db_name, schema, table_name, transformed_parquet, prim_key)
+    logging.info(
+        "Successfully loaded data into %s.%s.%s \n" % (db_name, schema, table_name)
+    )
+    os.remove(new_csv)
+    os.remove(old_csv)
+    os.rename(
+        transformed_parquet.strip("data_dump/"),
+        "metro_data_loaded_%s" % (datetime.date(datetime.today())),
+    )
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Description
 
-This is an ETL pipeline for taking `metro` data and loading it into the KWB data warehouse.
+This is an ETL pipeline for taking `metro` water depth data for monitoring wells and loading it into the KWB data warehouse.
 
 Once data is downloaded from `metro` in `.csv` format, the files are dumped in the `data_dump` folder. From here, the ETL is ran following morning via Task Scheduler and the Power Shell script.
 
@@ -8,9 +8,14 @@ Once data is downloaded from `metro` in `.csv` format, the files are dumped in t
 
 Currently this relies on one package, `kwb_loader`, which will install three dependies:
 
-
 1. `psychopg2`
 2. `polars`
 3. `numpy`
 
 Please see the requirements.txt file for specific versions.
+
+# Operations
+
+Data from `metro` contains Delivery data which we don't care about for the monitoring wells. To get around this, the file is read and everything past a line containing the word 'Delivery' is thrown out. From there, the date is added to the data for meta data purposes, the columns are renamed, and rows with no values in the measurement column are dropped. 
+
+The data is then written as a `.parquet` file and loaded into the database.
